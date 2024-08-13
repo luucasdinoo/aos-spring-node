cbc67bfafae05fd2de4b4b7125c58f803f81fbc8
Dino <luucasdinoo@gmail.com>
2024-08-12 21:58:19 -0300

    mysimpleapp

---
+++ mysimpleapp/src/main/java/br/com/dino/mysimpleapp/MysimpleappApplication.java
@@ -0,0 +1,13 @@
+package br.com.dino.mysimpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysimpleappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysimpleappApplication.class, args);
+    }
+
+}
---
+++ mysimpleapp/src/test/java/br/com/dino/mysimpleapp/MysimpleappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dino.mysimpleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysimpleappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
