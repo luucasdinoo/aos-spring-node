a4b956b49b8debc268f0fd2e450ab2726d18ee70
Dino <luucasdinoo@gmail.com>
2024-09-10 17:30:40 -0300

    Updating ConvertController

--- fahrenheintAndCelsius/src/main/java/com/convert/fahrenheintandcelsius/ConvertController.java
+++ fahrenheintAndCelsius/src/main/java/com/convert/fahrenheintandcelsius/ConvertController.java
@@ -2,20 +2,18 @@ package com.convert.fahrenheintandcelsius;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("convert")
 public class ConvertController {
 
-    @GetMapping("/celsiusToFahrenheint/{celsius}")
-    public Double celsiusToFahrenheint(@PathVariable Double celsius) {
-        return celsius * 1.8 + 32;
+    @GetMapping("/celsiusParaFahrenheit/{grau}")
+    public Double celsiusParaFahrenheit(@PathVariable Double grau) {
+        return grau * 1.8 + 32;
     }
 
-    @GetMapping("/fahrenheintToCelsius/{fahrenheint}")
-    public Double fahrenheintToCelsius(@PathVariable Double fahrenheint) {
-        return (fahrenheint - 32 ) / 1.8;
+    @GetMapping("/fahrenheitParaCelsius/{grau}")
+    public Double fahrenheitParaCelsius(@PathVariable Double grau) {
+        return (grau - 32 ) / 1.8;
     }
 }
