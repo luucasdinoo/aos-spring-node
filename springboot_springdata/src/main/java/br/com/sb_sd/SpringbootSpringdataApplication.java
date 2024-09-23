bdebe13feea699b71dabf9f2b4843ad0e310dbfe
Dino <luucasdinoo@gmail.com>
2024-09-23 16:49:31 -0300

    Spring Boot + Spring Data: primeira tentativa de deploy

---
+++ springboot_springdata/src/main/java/br/com/sb_sd/SpringbootSpringdataApplication.java
@@ -0,0 +1,13 @@
+package br.com.sb_sd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSpringdataApplication.class, args);
+    }
+
+}
---
+++ springboot_springdata/src/test/java/br/com/sb_sd/SpringbootSpringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sb_sd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpringdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
