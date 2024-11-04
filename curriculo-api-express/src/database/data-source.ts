14d26d72365afaa62d25e03790b83a0917f21778
Lucas Bernadino <luucasdinoo@gmail.com>
2024-11-03 22:45:23 -0300

    Vercel file

--- curriculo-api-express/src/database/data-source.ts
+++ curriculo-api-express/src/database/data-source.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { Education } from '@modules/education/entities/Education';
 import { Experience } from '@modules/experience/entities/Experience';
 import { PersonalData } from '@modules/personalData/entities/PersonalData';
@@ -5,12 +6,8 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'docker',
-  password: 'docker',
-  database: 'curriculo_db',
-  synchronize: true,
+  url: process.env.DATABASE_URL,
+  synchronize: false,
   logging: true,
   entities: [Education, Experience, PersonalData],
   //subscribers: [],
